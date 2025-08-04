be6ac8870985fe80475bb59a7c4e8ef47cd020d2
DarkLineRSA <ryanvantoorn@gmail.com>
2025-08-04 23:03:19 +0200

    Further refinements and tweaks

--- src/components/AppBar/AppBar.tsx
+++ src/components/AppBar/AppBar.tsx
@@ -2,8 +2,12 @@ import type { AppBarProps } from "./AppBar.types";
 import styles from "./AppBar.module.css";
 import MUIAppBar from "@mui/material/AppBar";
 
-export const AppBar: React.FC<AppBarProps> = ({ isVisible = true, extendedClass = "", inlineStyles = {} }) => {
+export const AppBar: React.FC<AppBarProps> = ({ children = <div></div>, isVisible = true, extendedClass = "", inlineStyles = {} }) => {
   if (!isVisible) return;
 
-  return <MUIAppBar className={`${styles.appBar} ${extendedClass}`} style={inlineStyles}></MUIAppBar>;
+  return (
+    <MUIAppBar className={`${styles.appBar} ${extendedClass}`} style={inlineStyles}>
+      {children}
+    </MUIAppBar>
+  );
 };
--- src/components/AppBar/AppBar.types.ts
+++ src/components/AppBar/AppBar.types.ts
@@ -1,6 +1,8 @@
 import type { BaseComponentProps } from "shared/StandardProps";
 import type { AppBarProps as MUIAppBarProps } from "@mui/material/AppBar";
+import type { ReactElement } from "react";
 
 export interface AppBarProps extends BaseComponentProps, MUIAppBarProps {
+  children: ReactElement;
   temp?: string;
 }
