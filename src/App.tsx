4d1b4c71414547096cfbfd6043acd9ec921cc190
DarkLineRSA <ryanvantoorn@gmail.com>
2025-08-04 23:05:54 +0200

    Addition of components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
-function App() {
-  return <></>;
-}
+import { AppBar } from "./components/AppBar/AppBar";
+import { Title } from "./components/Title/Title";
 
-export default App;
+export default function App() {
+  return (
+    <div className="app">
+      <div className="app-header">
+        <AppBar>
+          <Title title="TaskFlow" />
+        </AppBar>
+      </div>
+
+      <div className="app-body"></div>
+
+      <div className="app-footer"></div>
+    </div>
+  );
+}
